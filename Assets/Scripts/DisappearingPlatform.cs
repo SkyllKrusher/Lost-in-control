89cd78ccb3c7f1ab6fa3db47481123ae69e827fd
FUEL-HOME\Vivek <vivekjha92@gmail.com>
2020-02-01 21:52:42 +0530

    added disappearing platform script

---
+++ Assets/Scripts/DisappearingPlatform.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisappearingPlatform : MonoBehaviour
+{
+    [SerializeField] private GameObject opaquePlatform;
+    [SerializeField] private GameObject translucentPlatform;
+    [Space]
+    [SerializeField] private float disappearDelayTime = 2f;
+    [SerializeField] private float reappearDelayTime = 2f;
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        HandleCollision(col);
+    }
+
+    private void HandleCollision(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            StartCoroutine(DisappearAndReapperPlatform(disappearDelayTime));
+        }
+    }
+
+    private IEnumerator DisappearAndReapperPlatform(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        DisappearPlatform();
+        StartCoroutine(ReappearAfterDelay(reappearDelayTime));
+    }
+
+    private IEnumerator ReappearAfterDelay(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        ReappearPlatform();
+    }
+
+    private void DisappearPlatform()
+    {
+        opaquePlatform.SetActive(false);
+        translucentPlatform.SetActive(true);
+    }
+
+    private void ReappearPlatform()
+    {
+        opaquePlatform.SetActive(true);
+        translucentPlatform.SetActive(false);
+    }
+}
