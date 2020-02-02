7175522e454580bae94e98904eac8d52b06253a0
FUEL-HOME\Vivek <vivekjha92@gmail.com>
2020-02-02 06:22:42 +0530

    Resized Platforms, added Door script

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private bool hasKey;
+
+    private void Start()
+    {
+        //has key = manager value
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        HandleTriggerEnter(other);
+    }
+
+    private void HandleTriggerEnter(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (hasKey == true)
+            {
+                OpenDoor();
+            }
+        }
+    }
+
+    private void OpenDoor()
+    {
+        //manager next scene
+    }
+}
