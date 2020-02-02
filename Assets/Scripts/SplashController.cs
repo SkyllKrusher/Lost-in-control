10846af0f5d554c36f476af0fb8ebf451e5185ae
Sudiksha <sudiksha.sd1@gmail.com>
2020-02-02 01:37:39 -0800

    Added splash team name

--- Assets/Scripts/SplashController.cs
+++ Assets/Scripts/SplashController.cs
@@ -7,23 +7,31 @@ public class SplashController : MonoBehaviour
 {
     [SerializeField] private GameObject cvGameLogo;
     [SerializeField] private GameObject cvGgjLogo;
+    [SerializeField] private GameObject teamPanel;
     void Start()
     {
         StartCoroutine(StartSplash());
         cvGgjLogo.SetActive(false);
-        cvGameLogo.SetActive(true);
+        cvGameLogo.SetActive(false);
+        teamPanel.SetActive(true);
     }
 
     private IEnumerator StartSplash()
     {
         yield return StartCoroutine(FadeManager.Instance.BrightUp());
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(2f);
+        yield return StartCoroutine(FadeManager.Instance.BlackOut());
+        teamPanel.SetActive(false);
+        cvGameLogo.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        yield return StartCoroutine(FadeManager.Instance.BrightUp());
+        yield return new WaitForSeconds(1.5f);
         yield return StartCoroutine(FadeManager.Instance.BlackOut());
         cvGameLogo.SetActive(false);
         cvGgjLogo.SetActive(true);
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(1.5f);
         yield return StartCoroutine(FadeManager.Instance.BrightUp());
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(1.5f);
         yield return StartCoroutine(FadeManager.Instance.BlackOut());
         SceneManager.LoadScene(1);
     }
