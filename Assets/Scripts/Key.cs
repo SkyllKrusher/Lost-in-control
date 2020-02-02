0e798ad6192ad138c0d95a9ad3e30ec72cb34099
FUEL-HOME\Vivek <vivekjha92@gmail.com>
2020-02-02 06:38:09 +0530

    added key script

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        HandleTriggerEnter(other);
+    }
+
+    private void HandleTriggerEnter(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            CollectKey();
+        }
+    }
+
+    private void CollectKey()
+    {
+        //manager has key bool = true;
+    }
+}
